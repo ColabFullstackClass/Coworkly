9eb5ca0506e69d5dd2fd119ebe82e8fc0656e762
vickz185 <victorymathias185@gmail.com>
2026-01-20 22:09:42 +0100

    Update user controller, routes, index, package.json, add .gitignore

--- src/api/User/user.routes.js
+++ src/api/User/user.routes.js
@@ -1,4 +1,22 @@
-const router = require('express').Router();
+
+
+const express = require("express");
+const router = express.Router();
+const userController = require("./user.controller");
+
+
+router.get("/test", (req, res) => {
+  res.json({ message: "Auth routes working" });
+});
+
+// Register route
+router.post("/register", userController.registerUser);
+
+// Login route
+router.post("/login", userController.loginUser);
+
+// Refresh token route
+router.post("/refresh", userController.refreshToken);
 
 
 
